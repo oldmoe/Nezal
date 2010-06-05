c8fe4d2480e5974396d51562cc27b7824af7411d
rihamaldakkak <riham.aldakkak@gmail.com>
2010-06-05 14:57:28 +0300

    User Ranking page

--- studio/app/controllers/studio_controller.rb
+++ studio/app/controllers/studio_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class StudioController < ApplicationController 
   
   enable :sessions
@@ -11,8 +13,64 @@ class StudioController < ApplicationController
   
   get '/intro' do 
     puts "Inside get /intro"
+    @user.test(@fb_session_key)
     @matches = Match.dataset.all
     erb :studio, { :layout => false}
+  end
+  
+  get '/ranking/:round' do 
+    round = params["round"].to_sym
+    response = {:global_top_scorers => [], :friends_top_scorers => [], :global_ranking => [], :friends_ranking => [] }
+
+    @top_scorers = @user.global_top_scorers( round )
+    @friends_top_scorers = @user.friends_top_scorers(round)
+    @global_ranking = @user.global_ranking(round)
+    @friends_ranking = @user.among_friends_ranking(round)
+    
+    @top_scorers.each_index do | user |
+      hash = dump_user(@top_scorers[user], round)
+      hash["order"] = user + 1
+      response[:global_top_scorers] << hash
+    end
+        
+    @friends_top_scorers.each_index do | user |
+      hash = dump_user(@friends_top_scorers[user], round)
+      hash["order"] = user + 1
+      response[:friends_top_scorers] << hash
+    end
+    
+    list_length =  @global_ranking[:previous].length
+    @global_ranking[:previous].each_index do | user |
+      hash = dump_user(@global_ranking[:previous][user], round)
+      hash["order"] = @global_ranking[:rank] - list_length + user
+      response[:global_ranking] << hash
+    end
+    hash = dump_user(@user, round)
+    hash["order"] = @global_ranking[:rank]
+    response[:global_ranking] << hash
+    list_length =  @global_ranking[:next].length
+    @global_ranking[:next].each_index do | user |
+      hash = dump_user(@global_ranking[:next][user], round)
+      hash["order"] = @global_ranking[:rank] + user + 1
+      response[:global_ranking] << hash
+    end
+
+    list_length =  @friends_ranking[:previous].length    
+    @friends_ranking[:previous].each_index do | user |
+      hash = dump_user(@friends_ranking[:previous][user], round) 
+      hash["order"] = @friends_ranking[:rank] - list_length + user
+      response[:global_ranking] << hash
+    end
+    hash = dump_user(@user, round)
+    hash["order"] = @friends_ranking[:rank]
+    response[:friends_ranking] << hash
+    list_length =  @friends_ranking[:next].length
+    @friends_ranking[:next].each_index do | user |
+      hash = dump_user(@friends_ranking[:next][user], round)
+      hash["order"] = @friends_ranking[:rank] + user + 1
+      response[:friends_ranking] << hash
+    end
+    response.to_json
   end
   
   post '/predictions/:match_id' do
@@ -23,6 +81,21 @@ class StudioController < ApplicationController
     prediction.kicks_a = params['kicks_a']
     prediction.kicks_b = params['kicks_b']
     prediction.save
+  end
+  
+  def dump_user(user, round)
+    result = {}
+    result["id"] = user[:user_id]
+    result["name"] = user.user_info(@user.session)['first_name']
+    result["pic"] = user.user_info(@user.session)['pic_big']
+    result["score"] = user[round]
+    result["profile_url"] = user.user_info(@user.session)['profile_url']
+    result["me"] = if user[:user_id] == @user[:user_id]
+                    true 
+                  else
+                    false
+                  end
+    result
   end
   
 end
