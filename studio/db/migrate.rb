c0b9038ad4b789344140b693ff510ba11c86cd60
madhatter <madhatter@studio1.studio.nezal.com>
2010-06-10 20:39:26 +0300

    drop unwanted indexes

--- studio/db/migrate.rb
+++ studio/db/migrate.rb
@@ -4,10 +4,23 @@ require 'logger'
 
 DB = Sequel.sqlite('./db/studio.db', :logger => Logger.new('log/db.log'))
 
-p DB.schema( :matches)
-puts
-puts
-p DB.schema( :predictions)  
+indexes = DB.indexes(:users)
+p indexes
+
+indexes = DB.indexes(:predictions)
+p indexes
+
+indexes = DB.indexes(:comments)
+p indexes
+
+indexes = DB.indexes(:users)
+DB.drop_index :users, [:app_id, :global_score] if (indexes[:users_app_id_global_score_index])
+DB.drop_index :users, [:app_id, :finals_score] if (indexes[:users_app_id_finals_score_index])
+DB.drop_index :users, [:app_id, :semis_score] if (indexes[:users_app_id_semis_score_index])
+DB.drop_index :users, [:app_id, :quarters_score] if (indexes[:users_app_id_quarters_score_index])
+DB.drop_index :users, [:app_id, :round16_score] if (indexes[:users_app_id_round16_score_index])
+DB.drop_index :users, [:app_id, :first_round_score] if (indexes[:users_app_id_first_round_score_index])
+
 
 # == First Migration == #
 #DB.drop_column(:matches, :result_a)
