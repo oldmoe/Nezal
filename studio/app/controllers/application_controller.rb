e4a12be14016b4b8d19f103b72902647f6d08f92
oldmoe <oldmoe@gmail.com>
2010-06-20 16:54:37 +0300

    removed comments

--- studio/app/controllers/application_controller.rb
+++ studio/app/controllers/application_controller.rb
@@ -10,45 +10,18 @@ class ApplicationController < Sinatra::Base
   set :static, true
 
   before do 
-=begin
-    LOGGER.debug "BEGIN OF REQUEST"
-    LOGGER.debug ">>>>>> Referer :     #{env['HTTP_REFERER']}"
-    LOGGER.debug ">>>>>> Request Path : #{env['REQUEST_PATH']}"
-    LOGGER.debug ">>>>>> Request Path : #{env['rack.request.path']}"
-    LOGGER.debug ">>>>>> Query String : #{env["QUERY_STRING"]}"
-    LOGGER.debug ">>>>>> Cookie : #{env['HTTP_COOKIE']}"
-    LOGGER.debug ">>>>>> Rack Cookie : #{env['rack.request.cookie_hash']}"
-=end
     @app_configs = FB_CONFIGS::find('name', env['SCRIPT_NAME'].split('/')[1])
-=begin
 	if @app_configs && get_fb_session
-        @user = User[@app_configs['id'], @fb_uid] || User.create(:app_id => @app_configs['id'] , :user_id => @fb_uid, :global_score => 0, :first_round_score => 0, :round16_score => 0, :quarters_score => 0, :semis_score => 0, :finals_score => 0)
+       @user = User[@app_configs['id'], @fb_uid] || User.create(:app_id => @app_configs['id'] , :user_id => @fb_uid, :global_score => 0, :first_round_score => 0, :round16_score => 0, :quarters_score => 0, :semis_score => 0, :finals_score => 0)
         @user.session = @fb_session_key
 	else
       LOGGER.debug "No Cookie Or Params Found"
 	end
-=end
-#=begin	
-    if @app_configs && env['rack.request.cookie_hash'] && (fb_cookie = env['rack.request.cookie_hash']["fbs_#{@app_configs['id']}"] || env['rack.request.cookie_hash']["fbs_#{@app_configs['key']}"])
-        @cookie = CGI::parse(fb_cookie)
-        @fb_uid = @cookie['uid'][0].split('"')[0]
-        @fb_session_key = @cookie['session_key'][0]
-        LOGGER.debug ">>>>>> Cookie - uid : #{@fb_uid}"
-        LOGGER.debug ">>>>>> Cookie - session_key : #{@fb_session_key}"
-        @user = User[@app_configs['id'], @fb_uid] || User.create(:app_id => @app_configs['id'] , :user_id => @fb_uid, :global_score => 0, :first_round_score => 0, :round16_score => 0, :quarters_score => 0, :semis_score => 0, :finals_score => 0)
-        @user.session = @fb_session_key
-        p @user
-        p @user.session
-    else
-      puts "No Cookie Found"
-    end
-#=end
   end
   
   protected
     
   def get_fb_session
-#=begin
 	if env['rack.request.cookie_hash'] && (fb_cookie = env['rack.request.cookie_hash']["fbs_#{@app_configs['id']}"] || env['rack.request.cookie_hash']["fbs_#{@app_configs['key']}"])
 		cookie = CGI::parse(fb_cookie)
         @fb_uid = cookie['uid'][0].split('"')[0]
