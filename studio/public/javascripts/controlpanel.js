ba83df2c1c46e325b5c980525489a86aba0327d0
rihamaldakkak <riham.aldakkak@gmail.com>
2010-06-27 02:12:42 +0300

    fix dispearance of controlpanel on click

--- studio/public/javascripts/controlpanel.js
+++ studio/public/javascripts/controlpanel.js
@@ -2,15 +2,15 @@ var ControlPanel = {
     top : 0,
     
     toggle : function(){
-        state = $$('#controlpanel #title div')[0].getAttribute('class')
-        if(state == 'close')
+        state = $$('#controlpanel #title div')[1].getAttribute('class')
+        if(state == 'open off')
         {
-            $$('#controlpanel #title div')[0].setAttribute('class', 'open');
+            $$('#controlpanel #title div')[1].setAttribute('class', 'open on');
             $$('#controlpanel #panel-content')[0].style.top = '-190px';
             new Effect.Move('panel-content', {x:33, y: 46, mode: 'absolute', duration: 0.5, afterFinish : function(){ }})
             
         }else{
-            $$('#controlpanel #title div')[0].setAttribute('class', 'close');
+            $$('#controlpanel #title div')[1].setAttribute('class', 'open off');
             new Effect.Move('panel-content', {x:33, y: -190, mode: 'absolute', duration: 0.5, afterFinish : function(){ }})
         }
     },
