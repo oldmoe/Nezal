45e29a0bf799ce65dae96694c031159b12ecc704
rihamaldakkak <riham.aldakkak@gmail.com>
2010-06-27 02:18:28 +0300

    another small fix

--- studio/public/javascripts/controlpanel.js
+++ studio/public/javascripts/controlpanel.js
@@ -5,11 +5,13 @@ var ControlPanel = {
         state = $$('#controlpanel #title div')[1].getAttribute('class')
         if(state == 'open off')
         {
+            $$('#controlpanel #title div')[0].setAttribute('class', 'close off');
             $$('#controlpanel #title div')[1].setAttribute('class', 'open on');
             $$('#controlpanel #panel-content')[0].style.top = '-190px';
             new Effect.Move('panel-content', {x:33, y: 46, mode: 'absolute', duration: 0.5, afterFinish : function(){ }})
             
         }else{
+            $$('#controlpanel #title div')[0].setAttribute('class', 'close on');
             $$('#controlpanel #title div')[1].setAttribute('class', 'open off');
             new Effect.Move('panel-content', {x:33, y: -190, mode: 'absolute', duration: 0.5, afterFinish : function(){ }})
         }
