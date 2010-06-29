3b98d48c6d4407cb5c3b80d8a568e5fa692f8150
rihamaldakkak <riham.aldakkak@gmail.com>
2010-06-29 03:24:41 +0300

    add post remove app action

--- studio/app/controllers/users_controller.rb
+++ studio/app/controllers/users_controller.rb
@@ -7,5 +7,15 @@ class UsersController < ApplicationController
   get '/count' do 
     { "users" => DB[:users].filter(:app_id => @app_configs["id"]).count() }.to_json
   end
+  
+  post '/remove' do
+    p params
+    user = User[params['fb_sig_app_id'], params['fb_sig_user']]
+    user.delete() if ( user && params['fb_sig_uninstall'] )
+  end
 
+  post '/add' do
+    
+  end
+  
 end
