8e2c02ed8990dec4af34f086a8959097b743b13a
rihamaldakkak <riham.aldakkak@gmail.com>
2010-07-02 08:34:53 +0300

    display user picture in comments

--- studio/public/javascripts/game/comments.js
+++ studio/public/javascripts/game/comments.js
@@ -9,7 +9,8 @@ var Comments = {
     comments : [ ],
     
     warning : [[ { "id" : 0, 
-        "message" : "تنويه: نرجو من مستخدمي الموقع الكرام عدم إضافة أي تعليق يسيء للأديان , المعتقدات أو المقدسات. ونرجو عدم استخدام خدمة التعليقات في الترويج لأي إعلانات. كما نرجو ألا يتضمن التعليق السباب أو أي ألفاظ تخدش الحياء والذوق العام  "}]],
+        "message" : " نرجو من مستخدمي الموقع الكرام عدم إضافة أي تعليق يسيء للأديان , المعتقدات أو المقدسات. ونرجو عدم استخدام خدمة التعليقات في الترويج لأي إعلانات. كما نرجو ألا يتضمن التعليق السباب أو أي ألفاظ تخدش الحياء والذوق العام  ",
+        "name" : "تنويه"}]],
     
     sms : '',
         
@@ -66,10 +67,40 @@ var Comments = {
                                   onSuccess: function(t, json){
                                       var response = JSON.parse(t.responseText);
                                       Comments.comments = response["comments"].concat(Comments.comments).slice(0, 10)
-                                      Comments.sms = Comments.templates.sms[1].process({ msgs : Comments.comments.concat(Comments.warning)});
+                                      var user_ids = [];
+                                      for(var i=0; i< Comments.comments.length ; i++)
+                                      {
+                                          if ( ! Comments.comments[i][0]['name'])
+                                          {
+                                              user_ids[i] = Comments.comments[i][0]['user_id'];
+                                          }
+                                      }
+                                      console.log(user_ids)
+                                      FB.api(
+                                          {
+                                              method: 'users.getInfo',
+                                              uids : user_ids, 
+                                              fields : ['name', 'pic_square']
+                                          },
+                                          function(response) {
+                                              for(var i=0; i< response.length ; i++)
+                                              {
+                                                  for(var j=0; j< Comments.comments.length ;j++)
+                                                  {
+                                                      if(Comments.comments[j][0]['user_id'] == response[i]['uid'])
+                                                      {
+                                                          console.log(response[i]['name'])
+                                                          Comments.comments[j][0]['name'] = response[i]['name'];
+                                                          Comments.comments[j][0]['pic'] = response[i]['pic_square'];  
+                                                      }
+                                                  }
+                                              }
+                                              Comments.sms = Comments.templates.sms[1].process({ msgs : Comments.comments.concat(Comments.warning)});
+                                          }
+                                      );
                                 			if ( ! $$('#sms_marquee marquee').first() )
                                 			{
-                                			    window.setTimeout( Comments.refreshUpdate, 1000);
+                                			    window.setTimeout( Comments.refreshUpdate, 2000);
                   			          		}
                   			          		window.setTimeout( Comments.fetch, 20000 );
                                   }
