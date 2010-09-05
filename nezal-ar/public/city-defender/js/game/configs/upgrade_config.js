91d0dcc17059bdf0b80f7698cde2ed6321c6cd2f
rihamaldakkak <riham.aldakkak@gmail.com>
2010-09-05 21:41:33 +0200

    small change in description

--- nezal-ar/public/city-defender/js/game/configs/upgrade_config.js
+++ nezal-ar/public/city-defender/js/game/configs/upgrade_config.js
@@ -2,17 +2,17 @@
   This file is dedicated to describe upgrades
 */
 var UpgradeConfig = {
-    "bullets" : {name : "Bullets",
+    "Bullets" : {name : "Bullets",
                   image : 'upgrade1.png', 
                   smallImage : 'upgrade1.png', 
                   skeleton : 'upgrade1.png', 
                   desc : "Increases attack speed of all bullets towers by 50%." },
-    "rockets" : {    name : "Rockets",
+    "Rockets" : {    name : "Rockets",
                       image : 'upgrade2.png', 
                       smallImage : 'upgrade2.png', 
                       skeleton : 'upgrade2.png', 
                       desc : "Increases attack speed of all rocket towers by 50%." },
-    "shields" : {    name : "Shields",
+    "Shields" : {    name : "Shields",
                       image : 'upgrade3.png',
                       smallImage : 'upgrade3.png', 
                       skeleton : 'upgrade3.png', 
