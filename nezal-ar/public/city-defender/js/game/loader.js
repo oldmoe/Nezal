5212c42be6e412a523744910e3463fe9595e82b5
nomiero <hussein_nomier2010@yahoo.com>
2010-09-19 18:43:47 +0300

    load_challenge method for loader

--- nezal-ar/public/city-defender/js/game/loader.js
+++ nezal-ar/public/city-defender/js/game/loader.js
@@ -10,44 +10,54 @@ var Loader = {
 		else if(test.canPlayType('audio/ogg')){
 			this.soundsFormat = 'ogg'	
 		}							
-		this.toLoad = ["animations.html","challenges.html", "intro.html", this.soundsFormat+".html", "user.html", "background.html",  "game.html"]
+		this.toLoad = ["animations.html", "intro.html", this.soundsFormat+".html", "user.html", "background.html",  "game.html"]
+		this.soundIndex = this.toLoad.indexOf(this.soundsFormat+".html")
 	},
 
-	notify : function(win, resources){
+	notify : function(win, resources,callback){
 		var div = win.document.getElementById('resources')
 		var i =0
 		while(resources.length>i){
 			var resource = resources[i]
-			if(this.index==3)i++
+			if(this.index==this.soundIndex)i++
 			else resource = div.removeChild(resource)
 			var id = resource.id
 			var parts = id.split('#')
+			if(this.index>this.toLoad.length-1)console.log(parts[0],parts[1],parts[2])
 			if(!Loader[parts[0]][parts[1]])Loader[parts[0]][parts[1]] = {}
 			Loader[parts[0]][parts[1]][parts[2]] = resource; 
 		}
-		if(this.index == 3) createSounds()
-		if(this.index < this.toLoad.length-1){
+		if(this.index==this.soundIndex) createSounds()
+		if(this.index == this.toLoad.length-1){
 			this.index++
-			window.setTimeout(function(){$('iframe').src = "html_resources/"+Loader.toLoad[Loader.index]}, 500)
-		}
-		else{
 			if(development){
 				city_defender_start()
 				onFinish()
 			}
 			else Intro.show()
 		}
+		else if(this.index < this.toLoad.length-1){
+			this.index++
+			window.setTimeout(function(){$('iframe').src = "html_resources/"+Loader.toLoad[Loader.index]}, 500)
+		}
+		if(this.challengeCallback){
+			this.challengeCallback()
+			this.challengeCallback = null
+		}
+	},
+	
+	loadChallenge : function(challenge,callback){
+		this.challengeCallback = callback
+		$('iframe').src = "html_resources/"+challenge+".html"
 	}	
-
 }
-//new ResourceLoader().load()
 Loader.initialize()
 Loader.images ={}
 Loader.sounds = {}
 Loader.animations = {}
 Loader.challenges = {}
 Loader.resourceTypes = ['images', 'sounds','animations']
-Loader.index = 1
+Loader.index = 0
 
 
 
