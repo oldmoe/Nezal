c7a4c260efa3ff1d831fe2e3738f4674cb238676
rihamaldakkak <riham.aldakkak@gmail.com>
2010-09-30 22:15:39 +0300

    add configs to the new live app

--- nezal-ar/config.rb
+++ nezal-ar/config.rb
@@ -1,5 +1,5 @@
 worker_processes 8
-listen 5000
+listen 6000
 pid "tmp/pids/unicorn.pid"
 stderr_path "log/unicorn.stderr.log"
 stdout_path "log/unicorn.stdout.log"
--- nezal-ar/test-config.rb
+++ nezal-ar/test-config.rb
@@ -1,5 +1,5 @@
 worker_processes 4
-listen 6000
+listen 5000
 pid "tmp/pids/unicorn.pid"
 stderr_path "log/unicorn.stderr.log"
 stdout_path "log/unicorn.stdout.log"
