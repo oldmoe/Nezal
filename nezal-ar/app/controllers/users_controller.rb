c9c9502bca472c30727977b396a78243ea51e13e
rihamaldakkak <riham.aldakkak@gmail.com>
2010-09-30 22:11:57 +0300

    add configs to the new live app

--- nezal-ar/app/controllers/users_controller.rb
+++ nezal-ar/app/controllers/users_controller.rb
@@ -20,9 +20,14 @@ class UsersController < ApplicationController
     ''
   end
   
-  get '/remove' do
-    LOGGER.debug ">>>>>> Removing user from : " + @game.name  
+
+  post '/remove' do
+    @user = FbUser.where('fb_id' => params['fb_sig_user']).first
     if ( @user && params['fb_sig_uninstall'] )
+      @app_configs = FB_CONFIGS::find('id', params['fb_sig_app_id'])
+      @game = Game.where( 'name' => @app_configs['name']).first
+      @game_profile = UserGameProfile.where('game_id' => @game.id, 'user_id' => @user.id).first 
+      LOGGER.debug ">>>>>> Removing user from : " + @game.name  
       if @game_profile
         @game_profil.user_campaigns.destroy
         @game_profile.delete
