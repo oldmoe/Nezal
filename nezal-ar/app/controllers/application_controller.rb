c9b99dd5d8171745ab4b2003a4e11f15fe0a5fc0
nomiero <hussein_nomier2010@yahoo.com>
2010-10-17 20:34:39 +0200

    adding logs

--- nezal-ar/app/controllers/application_controller.rb
+++ nezal-ar/app/controllers/application_controller.rb
@@ -12,30 +12,30 @@ class ApplicationController < Sinatra::Base
     	if @app_configs && get_fb_session
     	  begin
 	        @game = Game.where( 'name' =>app_name).first
-          @user = FbUser.where( 'fb_id' => @fb_uid ).first
-          if(!@user)
-            @user = FbUser.create('fb_id' => @fb_uid )
-          end
-          @game_profile = UserGameProfile.where('game_id' => @game.id, 'user_id' => @user.id).first
-          if !(@game_profile)
-            LOGGER.debug "Game profile not found, creating one"
-            @game_profile = UserGameProfile.new()
-            @game_profile.game= @game
-            @game_profile.user= @user
-            get_helper_klass.init_game_profile(@game_profile)
-            @game_profile.save!()
-            puts params
-            puts params["inviter"]
-            if(params["inviter"])
-                get_helper_klass.reward_invitation(params["inviter"])
-            end
-          end
-        rescue Exception => e
+			@user = FbUser.where( 'fb_id' => @fb_uid ).first
+			if(!@user)
+				@user = FbUser.create('fb_id' => @fb_uid )
+			end
+			@game_profile = UserGameProfile.where('game_id' => @game.id, 'user_id' => @user.id).first
+			if !(@game_profile)
+				LOGGER.debug "Game profile not found, creating one"
+				@game_profile = UserGameProfile.new()
+				@game_profile.game= @game
+				@game_profile.user= @user
+				get_helper_klass.init_game_profile(@game_profile)
+				@game_profile.save!()
+				puts params
+				puts params["inviter"]
+				if(params["inviter"])
+					get_helper_klass.reward_invitation(params["inviter"])
+				end
+			end
+		  rescue Exception => e
           LOGGER.debug e
           ''
-        end
+		  end
 	    else
-        LOGGER.debug "No Cookie Or Params Found"
+          LOGGER.debug "No Cookie Or Params Found"
 	    end
     end
   end
@@ -49,33 +49,35 @@ class ApplicationController < Sinatra::Base
   protected
     
   def get_fb_session
-		 LOGGER.debug env.inspect
-	  if env['rack.request.cookie_hash'] && 
+	LOGGER.debug ">>>> env "+env['rack.request.cookie_hash']
+	LOGGER.debug ">>>> params "+params
+	if env['rack.request.cookie_hash'] && 
 	        (fb_cookie = env['rack.request.cookie_hash']["fbs_#{@app_configs['id']}"] ||
-           env['rack.request.cookie_hash']["fbs_#{@app_configs['key']}"])
-		    LOGGER.debug "cookie hash"
-		  cookie = CGI::parse(fb_cookie)
-      @fb_uid = cookie['uid'][0].split('"')[0]
-      @fb_session_key = cookie['session_key'][0]
-      LOGGER.debug ">>>>>> Cookie - uid : #{@fb_uid}"
-      LOGGER.debug ">>>>>> Cookie - session_key : #{@fb_session_key}"
-		  true
-	  elsif params[:fb_sig_session_key] && params[:fb_sig_user] && params['fb_sig_added'] == "1"
-	  LOGGER.debug "fb_sig_user"
-      @fb_uid = params[:fb_sig_user] 
-      @fb_session_key = params[:fb_sig_session_key]
-      LOGGER.debug ">>>>>> Params - uid : #{@fb_uid}"
-      LOGGER.debug ">>>>>> Params - session_key : #{@fb_session_key}"
-		  true
-	  elsif params[:session_key] && params[:uid] && params['fb_sig_added'] == "1"
-	  LOGGER.debug "fb_sig_added"
-      @fb_uid = params[:uid] 
-      @fb_session_key = params[:session_key]
-      LOGGER.debug ">>>>>> Our Params - uid : #{@fb_uid}"
-      LOGGER.debug ">>>>>> Our Params - session_key : #{@fb_session_key}"
-	  else
-		  false
-	  end
+           env['rack.request.cookie_hash']["fbs_#{@app_configs['key']}"]) # if
+		LOGGER.debug "cookie hash"
+		cookie = CGI::parse(fb_cookie)
+		@fb_uid = cookie['uid'][0].split('"')[0]
+		@fb_session_key = cookie['session_key'][0]
+		LOGGER.debug ">>>>>> Cookie - uid : #{@fb_uid}"
+		LOGGER.debug ">>>>>> Cookie - session_key : #{@fb_session_key}"
+		true
+	elsif params[:fb_sig_session_key] && params[:fb_sig_user] && params['fb_sig_added'] == "1"
+		LOGGER.debug "fb_sig_user"
+		@fb_uid = params[:fb_sig_user] 
+		@fb_session_key = params[:fb_sig_session_key]
+		LOGGER.debug ">>>>>> Params - uid : #{@fb_uid}"
+		LOGGER.debug ">>>>>> Params - session_key : #{@fb_session_key}"
+		true
+	elsif params[:session_key] && params[:uid] && params['fb_sig_added'] == "1"
+		LOGGER.debug "fb_sig_added"
+		@fb_uid = params[:uid] 
+		@fb_session_key = params[:session_key]
+		LOGGER.debug ">>>>>> Our Params - uid : #{@fb_uid}"
+		LOGGER.debug ">>>>>> Our Params - session_key : #{@fb_session_key}"
+		true
+	else
+		false
+	end
   end
   
   def get_helper_klass
