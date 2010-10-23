9f85b94a5fa9cc3b4467470f5778274c25d547ea
oldmoe <oldmoe@gmail.com>
2010-10-23 03:45:15 +0200

    fix for tutorial

--- nezal-ar/lib/resource_bundler.rb
+++ nezal-ar/lib/resource_bundler.rb
@@ -164,7 +164,8 @@ def generateDumbImageHTMLFile resourceDir,fileToWrite,id
 						image_id.sub!('/', '#').sub!('/', '#')							
 					end					
 				end
-				fileToWrite.puts "<img id='#{image_id}'  src='../#{image_id.gsub('#','/')}' />"
+				src = "../#{resourceDir.include? "animations" ? 'images/' : ''}#{image_id.gsub('#','/')}"
+				fileToWrite.puts "<img id='#{image_id}'  src='#{src}' />"
 			elsif File.directory? "#{resourceDir}/#{filename}"
 				generateDumbImageHTMLFile "#{resourceDir}/#{filename}",fileToWrite,id
 			end
