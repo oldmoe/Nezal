ff65a391bfa761738833cbf82063e4a83fd69c59
nomiero <hussein_nomier2010@yahoo.com>
2011-05-17 14:08:21 +0300

    merging

--- nezal-ar/public/base-defender/js/game/displays/building_display.js
+++ nezal-ar/public/base-defender/js/game/displays/building_display.js
@@ -86,7 +86,7 @@ var BuildingDisplay = Class.create(Display, {
         centered: true,
         shiftY: -10,
         styleClass: 'buildingName',
-        width : 140,
+        width : 144,
         shiftX : this.owner.imgWidth/2-70
       });
     else
