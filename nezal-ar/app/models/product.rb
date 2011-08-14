fae47b29e365068af26e953b7fe71e98ba89e849
Riham <riham.aldakkak@gmail.com>
2011-08-14 14:32:43 +0200

    bug fix

--- nezal-ar/app/models/product.rb
+++ nezal-ar/app/models/product.rb
@@ -2,7 +2,7 @@ class Product < DataStore::Model
 
   @@data = { 'fb' => { 'title'=>'', 'description'=>'', 'image_url'=>'', 'product_url'=>'' } }
 
-  def get(key)
+  def self.get(key)
     product = {
        'title' => 'Test',
        'price' => 10,
