f2f41b106d73dea6cd576ffe9e127c35548c4924
nomiero <hussein_nomier2010@yahoo.com>
2011-09-12 10:08:40 +0200

    merging

--- nezal-ar/public/newtsqaure/tsquare/js/game/gameData.js
+++ nezal-ar/public/newtsqaure/tsquare/js/game/gameData.js
@@ -3,6 +3,7 @@ var gameData =
 {"data":[[],[{"name":"journalist","category":"crowd","index":0,"lane":1,"x":0,"order":0},
     {"name":"journalist","category":"crowd","index":0,"lane":1,"x":1,"order":0},
     {"name":"journalist","category":"crowd","index":0,"lane":1,"x":2,"order":0},
+    {"name":"amn_markazy","category":"enemy","index":0,"lane":1,"x":10,"order":3},
    {
     "name": "block",
     "category": "enemy",
@@ -10,9 +11,9 @@ var gameData =
     "columns" : 2,
     "object" : "amn_markazy",
     "lane": 1,
-    "x": 15,
+    "x": 1000,
     "index": 0
-  },
-  {"name":"amn_markazy","category":"enemy","index":0,"lane":1,"x":1000,"order":3}]],
+  }
+  ]],
   "backgrounds":[[{"name":"skyline.png"}],[{"name":"skyline.png"}],[{"name":"sky1.png"}]],
   "environment":"day","gameModes":["normal"]}
