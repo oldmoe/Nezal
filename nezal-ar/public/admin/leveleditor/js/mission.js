6d527da4470d1034070b57edbd8b68fee932baeb
Ahmed Abousamra <ahmed.abousamra87@gmail.com>
2011-09-19 17:40:13 +0200

    level editor mission data bug

--- nezal-ar/public/admin/leveleditor/js/mission.js
+++ nezal-ar/public/admin/leveleditor/js/mission.js
@@ -7,8 +7,6 @@ var Mission = {
   id : null,
 
   initialize : function(){
-    Mission.currMission.data = gameData;
-    var levelLoader = new LevelLoader();
     
     this.id = parseInt(window.location.toString().split('?')[1].split('&')[0].split('=')[1]);
     this.game = window.location.toString().split('?')[1].split('&')[1].split('=')[1];
